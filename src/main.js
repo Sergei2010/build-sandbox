d2356e2d918e8e8291ed08b7a1765c72997f6dc9
sergei2010 <sergejmatvejcuk323@gmail.com>
2020-12-07 17:43:39 +0200

    Sandbox to play with React build tools

---
+++ src/main.js
@@ -0,0 +1,16 @@
+
+class App {
+
+   /* constructor() {
+        this.run = (name = 'World') => {
+            console.log(`Hello ${name}`);
+        }
+    }*/
+
+    run = (name = 'World') => {
+        console.log(`Hello ${name}`);
+    }
+}
+
+const app = new App();
+app.run();
